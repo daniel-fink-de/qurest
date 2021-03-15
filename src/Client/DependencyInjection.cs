c4603e3364909846a59e0c43c8a197fe8947b3bc
Daniel Fink <daniel-fink@outlook.com>
2021-03-15 22:48:34 +0800

    Added rest client

---
+++ src/Client/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace QuRest.Client
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddQuRestClient(this IServiceCollection services, Uri quRestUri)
+        {
+            services.AddHttpClient<QuRestClient>(httpClient => httpClient.BaseAddress = quRestUri);
+            return services;
+        }
+
+        public static IServiceCollection AddOpenApiClient(this IServiceCollection services, Uri quRestUri)
+        {
+            services.AddHttpClient<IQuRestClient, QuRestClient>(httpClient => httpClient.BaseAddress = quRestUri);
+            return services;
+        }
+    }
+}
